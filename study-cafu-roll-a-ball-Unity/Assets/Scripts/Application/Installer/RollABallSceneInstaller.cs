164a9bb407883c3f2fce3f681790d456a68c3f5a
mtmt <>
2019-07-26 04:47:29 +0900

    Scene Installer等の追加

---
+++ study-cafu-roll-a-ball-Unity/Assets/Scripts/Application/Installer/RollABallSceneInstaller.cs
@@ -0,0 +1,11 @@
+using Zenject;
+
+namespace StudyCafuRollABall.Application.Installer
+{
+    public class RollABallSceneInstaller : MonoInstaller<RollABallSceneInstaller>
+    {
+        public override void InstallBindings()
+        {
+        }
+    }
+}
