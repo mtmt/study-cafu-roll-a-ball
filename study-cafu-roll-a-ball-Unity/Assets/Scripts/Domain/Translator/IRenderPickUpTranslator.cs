0bc69617f19b9e8c2c49976dc6e135d9188743ba
mtmt <>
2019-07-28 02:01:32 +0900

    「`Pick Up`表示」translatorの追加

---
+++ study-cafu-roll-a-ball-Unity/Assets/Scripts/Domain/Translator/IRenderPickUpTranslator.cs
@@ -0,0 +1,10 @@
+using CAFU.Core;
+using StudyCafuRollABall.Domain.Entity;
+using StudyCafuRollABall.Domain.Structure;
+
+namespace StudyCafuRollABall.Domain.Translator
+{
+    public interface IRenderPickUpTranslator : ITranslator<IRenderPickUpEntity, IRenderPickUpStructure>
+    {
+    }
+}
